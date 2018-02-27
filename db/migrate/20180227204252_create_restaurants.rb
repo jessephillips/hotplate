a16799e8fe66809c0c91430a018bf6b21d1b2c98
jessephillips <mr.jesse.phillips@gmail.com>
2018-02-27 16:02:57 -0500

    Add validation on Restaurant name

--- db/migrate/20180227204252_create_restaurants.rb
+++ db/migrate/20180227204252_create_restaurants.rb
@@ -1,11 +1,11 @@
 class CreateRestaurants < ActiveRecord::Migration[5.1]
   def change
-    create_table :restaurants do |t|
-      t.string :name
-      t.string :address
-      t.string :website
-      t.string :phone
-      t.string :style
+    create_table  :restaurants do |t|
+      t.string    :name
+      t.string    :address
+      t.string    :website
+      t.string    :phone
+      t.string    :style
 
       t.timestamps
     end
