7dfa5e0997c6f24f074dbbb22b04159b2ee44352
jessephillips <mr.jesse.phillips@gmail.com>
2018-03-11 17:40:12 -0400

    Add #new, #create, #destroy actions for Meals.

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -10,4 +10,33 @@ class MealsController < ApplicationController
   def edit
     @meal = Meal.find(params[:id])
   end
+  
+  def update
+    @meal = Meal.find(params[:id])
+    @meal.update(_meal_params)
+    redirect_to meals_path
+  end
+  
+  def new
+    @meal = Meal.new
+  end
+  
+  def create
+    @meal = Meal.new(_meal_params)
+    @meal.save
+    redirect_to meals_path, notice: "#{@meal.name} created!"
+  end
+  
+  def destroy
+    @meal = Meal.find(params[:id])
+    @meal.destroy
+    redirect_to meals_path, notice: "#{@meal.name} successfully deleted!"
+  end
+
+private
+
+  def _meal_params
+    params.require(:meal).permit(:name, :cost_in_cents, :servings)
+  end
+
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,11 +2,21 @@ class Meal < ApplicationRecord
   validates :name,          presence: true
   validates :cost_in_cents, presence: true
   
+  before_save :_ensure_servings
+  
   def cost_in_dollars
     self.cost_in_cents / 100.0
   end
   
   def cost_per_serving
-    self.cost_in_cents / self.servings / 100.0
+    self.cost_in_cents / (self.servings ? self.servings : 1) / 100.0
   end
+  
+  private
+  
+    def _ensure_servings
+      unless self.servings.present?
+        self.servings = 1
+      end
+    end
 end
